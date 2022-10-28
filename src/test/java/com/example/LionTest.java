67b59b66e4ac0fd365d7dd675bd00723882e832e
nekirilova <alenameteneva@gmail.com>
2022-10-28 20:34:39 +0500

    add exception test

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,4 +1,16 @@
 package com.example;
 
+import org.junit.Test;
+
 public class LionTest {
+
+    IFeline feline;
+
+    @Test(expected = Exception.class)
+    public void constructorThrowsExceptionOnUnsupportedSex() throws Exception {
+
+        Lion lion = new Lion(feline, "Самка");
+      }
+
+
 }
