995f540a7dbfca2692c165b6b81cf256714ee293
nekirilova <alenameteneva@gmail.com>
2022-10-28 21:00:26 +0500

    add new tests to lionest

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,16 +1,50 @@
 package com.example;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LionTest {
-
+    //Сделать аннотацию before и вынести туда создание lion
     IFeline feline;
+
+    @Test
+    public void doesHaveManeSetMaleReturnsTrue() throws Exception {
+        Lion lion = new Lion(feline, "Самец");
+        boolean expectedResult = true;
+
+        boolean actualResult = lion.doesHaveMane();
+
+        Assert.assertEquals("Incorrect result", expectedResult, actualResult);
+    }
+
+    @Test
+    public void doesHaveManeSetFemaleReturnsFalse() throws Exception {
+        Lion lion = new Lion(feline, "Самка");
+        boolean expectedResult = false;
+
+        boolean actualResult = lion.doesHaveMane();
+
+        Assert.assertEquals("Incorrect result", expectedResult, actualResult);
+    }
 
     @Test(expected = Exception.class)
     public void constructorThrowsExceptionOnUnsupportedSex() throws Exception {
 
-        Lion lion = new Lion(feline, "Самка");
+        Lion lion = new Lion(feline, "Самочка");
+        lion.doesHaveMane();
       }
 
+      @Test
+    public void constructorExceptionTextIsCorrect() {
+        Exception exception = null;
+
+        try {
+            Lion lion = new Lion(feline, "Самочка");
+        } catch (Exception ex) {
+            exception = ex;
+        }
+
+          Assert.assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
+      }
 
 }
